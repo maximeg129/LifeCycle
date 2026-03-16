38fb15a3cebd5ba1b462f39ffb7a7fde47482adc
Maxime Galichet <maxime.galichet@gmail.com>
2026-03-16 14:34:40 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,8 @@
-'use client';
-
-// REMARQUE : Remplacez ces valeurs par celles fournies dans votre console Firebase
-// (Paramètres du projet > Général > Vos applications).
 export const firebaseConfig = {
-  apiKey: "AIzaSyAs-placeholder-key",
-  authDomain: "homly-vault.firebaseapp.com",
-  projectId: "homly-vault",
-  storageBucket: "homly-vault.firebasestorage.app",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef"
+  "projectId": "studio-3385001327-4400b",
+  "appId": "1:622545003442:web:2a80517446655631227aab",
+  "apiKey": "AIzaSyBi7hKnSpAFwl2nqjnZ3q36c3PJiaBI-wY",
+  "authDomain": "studio-3385001327-4400b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "622545003442"
 };
